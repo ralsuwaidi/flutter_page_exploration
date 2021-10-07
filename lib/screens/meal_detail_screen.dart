a657ef33d98d19e4536f43382756bef65fd97c93
Rashed Suwaidi <rashed@Rasheds-MacBook-Air.local>
2021-10-07 09:43:38 +0400

    added ingredients list

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
-  final String id;
+  final String mealId;
 
-  MealDetailScreen(this.id);
+  MealDetailScreen(this.mealId);
 
   @override
   Widget build(BuildContext context) {
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Title'),
+        title: Text('${selectedMeal.title}'),
       ),
       body: Column(
         children: [
           Container(
             height: 300,
             width: double.infinity,
-            child: Container(),
+            child: Image.network(selectedMeal.imageUrl, fit: BoxFit.cover),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text('Ingrediants',
+                style: Theme.of(context).textTheme.headline5),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            height: 200,
+            width: 300,
+            child: ListView.builder(
+              itemCount: selectedMeal.ingredients.length,
+              itemBuilder: (ctx, index) => Card(
+                color: Theme.of(context).colorScheme.secondary,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(selectedMeal.ingredients[index]),
+                ),
+              ),
+            ),
           ),
         ],
       ),
